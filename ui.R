ce4c340027107dc02d3624af896407e4c58492a3
Ayodeji Akande <ayodejiakande2107@gmail.com>
2024-07-19 16:24:50 +0100

    modify ui

--- ui.R
+++ ui.R
@@ -4,23 +4,24 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Predicting Iris Species"),
   sidebarPanel(
-    numericInput('id1', 'Numeric input, Sepal Length', 0, min = 0, max = 10),
-    numericInput('id2', 'Numeric input, Sepal Width', 0, min = 0, max = 10),
-    numericInput('id3', 'Numeric input, Petal Length', 0, min = 0, max = 10),
-    numericInput('id4', 'Numeric input, Petal Width', 0, min = 0, max = 10),
+    h3('Enter Explanatory values'),
+    numericInput('id1', 'Sepal Length', 0, min = 0, max = 10),
+    numericInput('id2', 'Sepal Width', 0, min = 0, max = 10),
+    numericInput('id3', 'Petal Length', 0, min = 0, max = 10),
+    numericInput('id4', 'Petal Width', 0, min = 0, max = 10),
     submitButton('Submit')
   ),
   mainPanel(
     h3('Results of prediction'),
-    h4('You entered'),
+    h4('You entered sepal length as:'),
     verbatimTextOutput("oid1"),
-    h4('You entered'),
+    h4('You entered sepal width as:'),
     verbatimTextOutput("oid2"),
-    h4('You entered'),
+    h4('You entered petal length as:'),
     verbatimTextOutput("oid3"),
-    h4('You entered'),
+    h4('You entered petal width as:'),
     verbatimTextOutput("oid4"),
-    h4('Which resulted in a prediction of '),
+    h4('The model predicted '),
     verbatimTextOutput("prediction")
   )
 ))
